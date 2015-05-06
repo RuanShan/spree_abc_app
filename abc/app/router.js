f4bf85126231604960677c718bc74aee98442ca7
zhang ze <areq22@gmail.com>
2015-05-06 00:34:04 -0700

    add spree-ember-store

--- abc/app/router.js
+++ abc/app/router.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
 import config from './config/environment';
+//This line imports the Spree Router.
+import spreeRouter from 'spree-ember-storefront/router';
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 export default Router.map(function() {
+    spreeRouter(this, {
+        mountPath:    '/spree',
+        cartPath:     'cart',
+        productsPath: 'products',
+        checkoutPath: 'checkout'
+      });
+
 });
